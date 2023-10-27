1dd0b9929ccc23d0140079cc607921fb897efe1f
dsildaikin <dsildaikin@gmail.com>
2023-10-28 00:43:51 +0300

    add test validate and add methods

--- todo_app_sqlite/lib/screens/detail_screen.dart
+++ todo_app_sqlite/lib/screens/detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_sqlite/db/database.dart';
+import 'package:todo_app_sqlite/model/todo.dart';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -8,19 +10,36 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
-  final _nameController = TextEditingController();
-  final _timeController = TextEditingController();
-  final _dateController = TextEditingController();
-  final _importanceDegreeController = TextEditingController();
-  final _noteController = TextEditingController();
+  int? todoIdForUpdate;
+
+  late String _todoName;
+  late String _todoTime;
+  late String _todoDate;
+  late String _todoImportanceDegree;
+  late String _todoNote;
+  late String _todoCompleted;
+
+  late Future<List<Todo>> _todosList;
+
+  bool isUpdate = false;
+
+  final _formStateKey = GlobalKey<FormState>();
+
+  final _todoNameController = TextEditingController();
+  final _todoTimeController = TextEditingController();
+  final _todoDateController = TextEditingController();
+  final _todoImportanceDegreeController = TextEditingController();
+  final _todoNoteController = TextEditingController();
+  final _todoCompletedController = TextEditingController();
 
   @override
   void dispose() {
-    _nameController.dispose();
-    _timeController.dispose();
-    _dateController.dispose();
-    _importanceDegreeController.dispose();
-    _noteController.dispose();
+    _todoNameController.dispose();
+    _todoTimeController.dispose();
+    _todoDateController.dispose();
+    _todoImportanceDegreeController.dispose();
+    _todoNoteController.dispose();
+    _todoCompletedController.dispose();
     super.dispose();
   }
 
@@ -32,11 +51,12 @@ class _DetailScreenState extends State<DetailScreen> {
         centerTitle: true,
       ),
       body: Form(
+        key: _formStateKey,
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: [
             TextFormField(
-              controller: _nameController,
+              controller: _todoNameController,
               decoration: const InputDecoration(
                 labelText: 'Название',
                 hintText: 'Введите название',
@@ -47,10 +67,22 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
                 border: OutlineInputBorder(),
               ),
+              validator: (value) {
+                if (value == null) {
+                  return 'Please Enter Todo Name';
+                }
+                if (value.trim() == "") {
+                  return "Only Space is Not Valid!!!";
+                }
+                return null;
+              },
+              onSaved: (value) {
+                _todoName = value!;
+              },
             ),
             const SizedBox(height: 10),
             TextFormField(
-              controller: _timeController,
+              controller: _todoTimeController,
               decoration: const InputDecoration(
                 labelText: 'Время',
                 hintText: 'Введите время',
@@ -64,7 +96,7 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
             const SizedBox(height: 10),
             TextFormField(
-              controller: _dateController,
+              controller: _todoDateController,
               decoration: const InputDecoration(
                 labelText: 'Дата',
                 hintText: 'Введите дату',
@@ -78,7 +110,7 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
             const SizedBox(height: 10),
             TextFormField(
-              controller: _importanceDegreeController,
+              controller: _todoImportanceDegreeController,
               decoration: const InputDecoration(
                 labelText: 'Степень важности',
                 hintText: 'Введите степень важности',
@@ -92,7 +124,7 @@ class _DetailScreenState extends State<DetailScreen> {
             ),
             const SizedBox(height: 10),
             TextFormField(
-              controller: _noteController,
+              controller: _todoNoteController,
               maxLines: 3,
               decoration: const InputDecoration(
                 labelText: 'Примечание',
@@ -105,9 +137,97 @@ class _DetailScreenState extends State<DetailScreen> {
                 border: OutlineInputBorder(),
               ),
             ),
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: _todoCompletedController,
+              decoration: const InputDecoration(
+                labelText: 'Статус',
+                hintText: 'Введите статус дела',
+                prefixIcon: Icon(Icons.done),
+                suffixIcon: Icon(
+                  Icons.delete_outline,
+                  color: Colors.red,
+                ),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _addOrUpdateTodo,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: const Text('Добавить'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  child: const Text('Очистить'),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void _addOrUpdateTodo() {
+    if (isUpdate) {
+      if (_formStateKey.currentState!.validate()) {
+        _formStateKey.currentState!.save();
+        DBProvider.db
+            .updateTodo(
+          Todo(
+            todoIdForUpdate!,
+            _todoName,
+            _todoTime,
+            _todoDate,
+            _todoImportanceDegree,
+            _todoNote,
+            _todoCompleted,
+          ),
+        )
+            .then((data) {
+          setState(() {
+            isUpdate = false;
+          });
+        });
+      }
+    } else {
+      if (_formStateKey.currentState!.validate()) {
+        _formStateKey.currentState!.save();
+        DBProvider.db.insertTodo(
+          Todo(
+            null,
+            _todoName,
+            _todoTime,
+            _todoDate,
+            _todoImportanceDegree,
+            _todoNote,
+            _todoCompleted,
+          ),
+        );
+      }
+    }
+    _todoNameController.text = '';
+    _todoTimeController.text = '';
+    _todoDateController.text = '';
+    _todoImportanceDegreeController.text = '';
+    _todoNoteController.text = '';
+    _todoCompletedController.text = '';
+
+    updateTodoList();
+  }
+
+  updateTodoList() {
+    setState(() {
+      _todosList = DBProvider.db.getTodos();
+    });
   }
 }
